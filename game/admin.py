7df42853fdb61c1433dacf115219ba3d190c1998
Samuel Dantas <samuel.v.dantas@gmail.com>
2020-06-01 01:44:16 -0300

    add save model to a generic class to use it by inheritance

--- game/admin.py
+++ game/admin.py
@@ -4,30 +4,27 @@ from django.contrib.auth.models import Group
 from game.models import Book, Question, Choice
 
 
-class BookAdmin(admin.ModelAdmin):
-    exclude = ('user',)
-    list_display = ('title', 'description', 'user')
-
+class GameModelAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         obj.user = request.user
         obj.last_modifield_by = request.user
         obj.save()
+
+
+class BookAdmin(GameModelAdmin):
+    exclude = ('user',)
+    list_display = ('title', 'description', 'user')
 
 
 class ChoicesInline(admin.TabularInline):
     model = Choice
 
 
-class QuestionAdmin(admin.ModelAdmin):
+class QuestionAdmin(GameModelAdmin):
     inlines = [ChoicesInline]
     exclude = ('user',)
     list_display = ('text', 'book', 'pub_date', 'user')
     list_filter = ('text', 'book', 'user')
-
-    def save_model(self, request, obj, form, change):
-        obj.user = request.user
-        obj.last_modifield_by = request.user
-        obj.save()
 
 
 admin.site.site_header = 'Gameficação na leitura'
