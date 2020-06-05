fbfc9fc3c32c536243c3559d2757feb3f83ac5eb
Samuel Dantas <samuel.v.dantas@gmail.com>
2020-06-05 01:09:52 -0300

    add test

--- game/tests.py
+++ game/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from game.models import Book, Question, Choice, Answer, User
+
+
+class BookTest(TestCase):
+    def setUp(self):
+        myuser = User.objects.first()
+        Book.objects.create(title='test_1', author='samuel',
+                            description='teste de descricao', user=myuser)
+
+    def test_book_exists(self):
+        book = Book.objcts.get(name='test_1')
+        self.assertIsNotNone(book)
